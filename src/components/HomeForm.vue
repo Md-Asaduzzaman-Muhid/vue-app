b387cd5d5523a6c6df7fe7b2f7918b44099b16e7
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-01-20 18:20:44 +0600

    axios added

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -1,44 +1,72 @@
 <template>
   <!-- Default form contact -->
-  <form id="user-form" action="http://localhost:8000/api/users" method="post">
+  <form id="user-form" @submit.prevent= "postValue()">
     <p class="h4 text-center mb-4">Add user info: </p>
     <!-- Default input name -->
     <label for="defaultFormContactNameEx" class="grey-text">Your name</label>
-    <input type="text" id="defaultFormContactNameEx" class="form-control" name="name">
+    <input type="text" id="defaultFormContactNameEx" class="form-control" name="name" v-model="name">
     <br>
 
     <!-- Default input phone -->
     <label for="defaultFormContactPhoneEx" class="grey-text">Your Phone</label>
-    <input type="phone" id="defaultFormContactPhoneEx" class="form-control" name="phone">
+    <input type="phone" id="defaultFormContactPhoneEx" class="form-control" name="phone" v-model="phone">
 
     <br>
 
     <!-- Default input email -->
     <label for="defaultFormContactEmailEx" class="grey-text">Your email</label>
-    <input type="email" id="defaultFormContactEmailEx" class="form-control" name="email">
+    <input type="email" id="defaultFormContactEmailEx" class="form-control" name="email" v-model="email">
 
     <br>
 
     <!-- Default input ImageURL -->
     <label for="defaultFormContactImageURLtEx" class="grey-text">Image URL</label>
-    <input type="text" id="defaultFormContactImageURLEx" class="form-control" name="image_url">
+    <input type="text" id="defaultFormContactImageURLEx" class="form-control" name="image_url" v-model="image_url">
 
     <br>
 
     <div class="text-center mt-4">
-        <button class="btn btn-outline-warning" type="submit">Send<i class="far fa-paper-plane ml-2"></i></button>
+        <button class="btn btn-outline-warning" type="submit">Send Value<i class="far fa-paper-plane ml-2"></i></button>
     </div>
   </form>
   <!-- Default form contact -->
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HomeFrom',
-  props: {
-    msg: String
+  data(){
+    return{
+      name: '',
+      phone: '',
+      email: '',
+      image_url: ''
+    }
+  },
+  // props: {
+  //   msg: String
+  // },
+  methods:{
+    postValue(){
+      axios.post('http://localhost:8000/api/users',{
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        image_url: this.image_url
+      }
+    ) .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
